78ae3ccd4fdda186ce1eedbc5e0ebe8780ddf2c5
begopla <begonapr@gmail.com>
2022-09-03 15:44:50 +0400

    last version

--- server/routes/index.routes.js
+++ server/routes/index.routes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
-const phonesRoutes = require("./phones.routes");
 
+const phonesRoutes = require("./phones.routes");
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.json("All good in here");
